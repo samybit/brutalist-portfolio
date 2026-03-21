c25cb285aafba029a0e22fdce9382f47f483a14b
samybit <samyb.samir@gmail.com>
2026-03-21 17:36:43 +0200

    install react-animated-cursor tailwind-scrollbar --legacy-peer-deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,5 +69,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("tailwind-scrollbar")({ nocompatible: true }),
+  ],
 }
